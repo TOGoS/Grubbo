d2f9248e7b7b86e80f14d5d84bd13cfc92424b63
Dan Stevens <stevens@earthit.com>
2009-11-22 21:00:05 -0600

    Note of some re-architecting to be done in Resource.php

--- lib/Grubbo/Value/Resource.php
+++ lib/Grubbo/Value/Resource.php
@@ -2,4 +2,8 @@
 
 interface Grubbo_Value_Resource {
     function getContentMetadata();
+    // TODO: 'resource' should not be the same object
+    // as its content.  Change Blob, Directory to not
+    // be subclasses of Resource, and instead give
+    // Resource a 'content' property, which can reference anything.
 }
