2d482d408872eef777e4bfa70ce51daa44b89b58
Dan Stevens <stevens@earthit.com>
2009-11-23 13:24:16 -0600

    Added module field for new tickets, and column on filter page (http://grubbo.earthit.com/grubbo/tickets/10).

--- themes/default/templates/edit-page.php
+++ themes/default/templates/edit-page.php
@@ -8,6 +8,7 @@ $docTitle = $metadata['doc/title'] or $docTitle = 'Some Page';
 $isTicket = $metadata['doc/ticket'];
 $ticketStatus = $metadata['doc/status'];
 $assignedTo = $metadata['doc/assigned-to'];
+$module = $metadata['doc/module'];
 $cc = $metadata['doc/cc'];
 foreach( $metadata as $k=>$v ) {
     if( $k == 'doc/title' or $k == 'doc/cc' or $k == 'doc/assigned-to' ) {
@@ -24,11 +25,12 @@ foreach( $metadata as $k=>$v ) {
 <?php foreach( $hiddenMetadata as $k=>$v ) { ?>
 <input type="hidden" name="<?php echo htmlspecialchars($k); ?>" value="<?php echo htmlspecialchars($v); ?>"/>
 <?php } ?>
+<tr><th>Title</th><td><input type="text" name="doc/title" value="<?php echo htmlspecialchars($docTitle); ?>" size="60"/></td></tr>
 <?php if($isTicket) { ?>
 <tr><th>Assign to</th><td><input type="text" name="doc/assigned-to" value="<?php echo htmlspecialchars($assignedTo); ?>" size="60"/></td></tr>
 <tr><th>Status</th><td><?php echo $this->formatDropdown('doc/status', $ticketStatusOptions, $ticketStatus); ?></td></tr>
+<tr><th>Module</th><td><input type="text" name="doc/module" value="<?php echo htmlspecialchars($module); ?>" size="60"/></td></tr>
 <?php } ?>
-<tr><th>Title</th><td><input type="text" name="doc/title" value="<?php echo htmlspecialchars($docTitle); ?>" size="60"/></td></tr>
 <tr><th>CC</th><td><input type="text" name="doc/cc" value="<?php echo htmlspecialchars($cc); ?>" size="60"/></td></tr>
 <tr><td colspan="2">
 <textarea name="content" rows="20" cols="80"><?php echo htmlspecialchars($resource->getContent()); ?></textarea><br />
--- themes/default/templates/filter-tickets.php
+++ themes/default/templates/filter-tickets.php
@@ -1,9 +1,10 @@
 <?php $this->outputTemplate('page-header',array('pageTitle'=>$pageTitle)); ?>
 
 <p style="color:red">TODO: put form with filtering options here.</p>
+<p><a href="new-ticket">Create new ticket</a></p>
 
 <table>
-<tr><th>#</th><th>Status</th><th>Assigned</th><th>Title</th></tr>
+<tr><th>#</th><th>Module</th><th>Status</th><th>Assigned</th><th>Title</th></tr>
 <?php
 
 foreach( $tickets as $name=>$target ) {
@@ -17,6 +18,7 @@ foreach( $tickets as $name=>$target ) {
 
     echo "<tr>";
     echo "<td><a href=\"", htmlspecialchars($href), "\">", htmlspecialchars($href), "</a></td>";
+    echo "<td>", htmlspecialchars($md['doc/module']), "</td>";
     echo "<td>", htmlspecialchars($md['doc/status']), "</td>";
     echo "<td>", htmlspecialchars($md['doc/assigned-to']), "</td>";
     echo "<td><a href=\"", htmlspecialchars($href), "\">", htmlspecialchars($md['doc/title']), "</a></td>";
