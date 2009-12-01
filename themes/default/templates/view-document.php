71b540373f240dfc3b11a90e2b1caa52c2aee6ed
Dan Stevens <stevens@earthit.com>
2009-11-30 23:15:23 -0600

    Open tickets / new ticket link on view ticket page

--- themes/default/templates/view-document.php
+++ themes/default/templates/view-document.php
@@ -13,6 +13,7 @@ $status = $rmd['doc/status'];
 <?php $this->outputTemplate('page-header',array('pageTitle'=>$title)); ?>
 
 <?php if($isTicket) { ?>
+<p><a href="my-open-tickets">My open tickets</a> | <a href="new-ticket">Create new ticket</a></p>
 <table class="form-pairs">
 <tr><th>Assigned to</th><td><?php echo htmlspecialchars($assignedTo); ?></td></tr>
 <tr><th>Status</th><td><?php echo htmlspecialchars($status); ?></td></tr>
